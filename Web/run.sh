b54308ba5cf96bbf522d902da2ec8ff53365ca11
root <root@prod4.ontomics.com>
2014-12-30 14:37:49 -0500

    running custom cabot image

--- Web/run.sh
+++ Web/run.sh
@@ -9,4 +9,4 @@ export SES_PORT="225"
 
 service nginx restart &&\
 gunicorn cabot.wsgi:application --config gunicorn.conf --log-level info --log-file /var/log/gunicorn &\
-celery worker -B -A cabot --loglevel=INFO --concurrency=16 -Ofair
+celery worker -B -A cabot --loglevel=INFO --concurrency=8 -Ofair
