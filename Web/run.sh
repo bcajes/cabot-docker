49f2226f64297104668ea80ee18e50ebbdf5729c
root <root@prod4.ontomics.com>
2014-12-31 17:33:15 -0500

    added ping utils

--- Web/run.sh
+++ Web/run.sh
@@ -2,11 +2,7 @@
 
 export DATABASE_URL="postgres://docker:docker@$DATABASE_POSTGRE_HOST:5432/docker"
 export CELERY_BROKER_URL="redis://$CELERY_BROKER_REDIS_HOST:6379/1"
-export SES_HOST="$MAIL_EXIM_HOST"
-export SES_USER=""
-export SES_PASS=""
-export SES_PORT="225"
 
 service nginx restart &&\
 gunicorn cabot.wsgi:application --config gunicorn.conf --log-level info --log-file /var/log/gunicorn &\
-celery worker -B -A cabot --loglevel=INFO --concurrency=8 -Ofair
+celery worker -B -A cabot --loglevel=INFO --concurrency=4 -Ofair
