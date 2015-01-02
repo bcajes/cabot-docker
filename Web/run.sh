786db95ed078a097c88e6544a02ff6988f676712
root <root@prod4.ontomics.com>
2015-01-02 09:51:35 -0500

    trying ep for nginx templating

--- Web/run.sh
+++ Web/run.sh
@@ -3,6 +3,6 @@
 export DATABASE_URL="postgres://docker:docker@$DATABASE_POSTGRE_HOST:5432/docker"
 export CELERY_BROKER_URL="redis://$CELERY_BROKER_REDIS_HOST:6379/1"
 
-#service nginx restart &&\
+service nginx restart &&\
 gunicorn cabot.wsgi:application --config gunicorn.conf --log-level info --log-file /var/log/gunicorn &\
 celery worker -B -A cabot --loglevel=INFO --concurrency=4 -Ofair
